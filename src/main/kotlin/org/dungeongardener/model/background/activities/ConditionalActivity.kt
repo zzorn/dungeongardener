7922344bdb4295a0b95cfd1e81807f544263179c
zzorn <zzorn@iki.fi>
2016-02-12 17:20:07 +0200

    Bit of cleanup

--- src/main/kotlin/org/dungeongardener/model/background/activities/ConditionalActivity.kt
+++ src/main/kotlin/org/dungeongardener/model/background/activities/ConditionalActivity.kt
@@ -3,19 +3,19 @@ package org.dungeongardener.model.background.activities
 import org.dungeongardener.model.background.BackgroundCallback
 import org.dungeongardener.model.creature.Creature
 import org.dungeongardener.util.Context
-import org.dungeongardener.util.boolexpr.BoolExpr
+import org.dungeongardener.util.genlang.nodes.Expression
 
 /**
  * Only do the nested activity if the conditions are met
  */
 class ConditionalActivity(val activityOnSuccess: Activity,
                           val activityOnFailure: Activity?,
-                          val condition: BoolExpr,
+                          val condition: Expression,
                           override val name: String = "Condition ${condition}") : Activity {
 
     override fun enter(character: Creature, callback: BackgroundCallback, context: Context): Boolean {
 
-        val success = condition.evaluate(context)
+        val success = condition.evaluate<Boolean>(context)
 
         if (success) {
             activityOnSuccess.enter(character, callback, context)
